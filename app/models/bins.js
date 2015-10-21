315030d5d2a3d6e9f2e6731117997c4a22e3e56b
Harshita Kasera <harshita.k@flipkart.com>
2015-10-21 22:44:24 +0530

    Formatted JS

--- app/models/bins.js
+++ app/models/bins.js
@@ -1,76 +1,73 @@
 var db = require("../../db.js");
 var mongojs = require('mongojs');
-var ObjectId = mongojs.ObjectId; 
+var ObjectId = mongojs.ObjectId;
 module.exports = {
-	getAllBins: function(req, res){  
-		var bins = db.collection('bins');
-   		db.bins.find(
-   			{},
-   			{
-   				"isActive": true,
-    			"latitude": true,
-    			"longitude":true,
-          "humidity":true,
-          "temperature":true,
-          "fill":true,
-          "name":true,
-          "type":true,
-          "address":true
-    		},function(err, docs) {
-          if(!err){
-            res.send(docs);  
-          }else{
-            res.send({});
-          }
-	       	
-	    }); 
-	},
-  getBinInLocation:function(req,res){
-    var bins = db.collection('bins'), 
-        lati = parseFloat(req.params.lati),
-        longi = parseFloat(req.params.longi),
-        maxDistance = 2000;
+    getAllBins: function(req, res) {
+        var bins = db.collection('bins');
+        db.bins.find({}, {
+            "isActive": true,
+            "latitude": true,
+            "longitude": true,
+            "humidity": true,
+            "temperature": true,
+            "fill": true,
+            "name": true,
+            "type": true,
+            "address": true
+        }, function(err, docs) {
+            if (!err) {
+                res.send(docs);
+            } else {
+                res.send({});
+            }
 
-    if(req.params.radius){
-      maxDistance = parseInt(req.params.radius,10);
+        });
+    },
+    getBinInLocation: function(req, res) {
+        var bins = db.collection('bins'),
+            lati = parseFloat(req.params.lati),
+            longi = parseFloat(req.params.longi),
+            maxDistance = 2000;
+
+        if (req.params.radius) {
+            maxDistance = parseInt(req.params.radius, 10);
+        }
+        bins.find({
+            loc: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [longi, lati]
+                    },
+                    $maxDistance: maxDistance
+                }
+            }
+        }).toArray(function(err, docs) {
+            if (err) return console.dir(err)
+
+            res.send(docs);
+        });
+    },
+    getBinDetails: function(req, res) {
+        db.bins.find({
+            "_id": ObjectId(req.params.id)
+        }, {
+            "isActive": true,
+            "latitude": true,
+            "longitude": true,
+            "humidity": true,
+            "temperature": true,
+            "fill": true,
+            "name": true,
+            "type": true,
+            "address": true
+        }, function(err, docs) {
+            if (!err) {
+                res.send(docs);
+            } else {
+                res.send({});
+            }
+
+        });
     }
-    bins.find(
-      {
-        loc : {
-      $near : {
-        $geometry : { 
-          type : "Point" , 
-          coordinates : [longi,lati]  
-        }, 
-        $maxDistance : maxDistance
-      }
-    }}).toArray(function(err, docs) {
-      if(err) return console.dir(err)
-
-      res.send(docs);  
-    });
-  },
-  getBinDetails:function(req,res){
-    db.bins.find(
-        {"_id": ObjectId(req.params.id)  },
-        {
-          "isActive": true,
-          "latitude": true,
-          "longitude":true,
-          "humidity":true,
-          "temperature":true,
-          "fill":true,
-          "name":true,
-          "type":true,
-          "address":true
-        },function(err, docs) {
-          if(!err){
-            res.send(docs);  
-          }else{
-            res.send({});
-          }
-          
-      }); 
-  }
 }
-
