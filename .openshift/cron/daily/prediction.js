66006553d996dfd21b9fa1df0479c9e00e71d8dd
Harshita Kasera <harshita.k@flipkart.com>
2015-10-22 22:58:27 +0530

    Fix in path directory

--- .openshift/cron/daily/prediction.js
+++ .openshift/cron/daily/prediction.js
@@ -1,9 +1,9 @@
 #!/bin/bash
 if(process.env.OPENSHIFT_DATA_DIR){
 
-	var Bins = require(process.env.OPENSHIFT_DATA_DIR+"/app/models/bins.js");
-	var BinsActivity = require(process.env.OPENSHIFT_DATA_DIR+"/app/models/binsActivity.js");
-	var Utils = require(process.env.OPENSHIFT_DATA_DIR+"/app/common/utils.js");
+	var Bins = require(process.env.OPENSHIFT_DATA_DIR+"app/models/bins.js");
+	var BinsActivity = require(process.env.OPENSHIFT_DATA_DIR+"app/models/binsActivity.js");
+	var Utils = require(process.env.OPENSHIFT_DATA_DIR+"app/common/utils.js");
 
 	BinsActivity.binPredictionCalc({});
 }
