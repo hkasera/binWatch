257451a5a35c18c39660f8ec177728170eb50305
Harshita Kasera <harshita.k@flipkart.com>
2015-10-22 23:01:47 +0530

    Removed cron job

--- .openshift/cron/daily/prediction.js
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if(process.env.OPENSHIFT_DATA_DIR){
-
-	var Bins = require(process.env.OPENSHIFT_DATA_DIR+"app/models/bins.js");
-	var BinsActivity = require(process.env.OPENSHIFT_DATA_DIR+"app/models/binsActivity.js");
-	var Utils = require(process.env.OPENSHIFT_DATA_DIR+"app/common/utils.js");
-
-	BinsActivity.binPredictionCalc({});
-}
